f30b39240e0aa4c8c89c3cc8e0b68c01321d06f2
이현욱 <hjhsha827@gmail.com>
2019-09-04 12:07:19 +0900

    ADD
    성공하지 못했습니다...

---
+++ 이현욱/20190904/File.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by 이현욱 on 04/09/2019.
+//
+
+import Foundation
+
+func solution( _ arr : [Int]) -> Int {
+    var answer = lcm(arr[0], arr[1])
+    
+    for i in 2...arr.count {
+        answer = lcm(answer, arr[i])
+    }
+    return answer
+}
+
+func gcd( _ a : Int, _ b : Int) -> Int {
+    while(b != 0) {
+        var temp = a % b
+        a = b
+        b = temp
+    }
+    return a
+}
+
+func lcm( _ a : Int, _ b : Int) -> Int {
+    return Int(a * b / gcd(a,b))
+}
