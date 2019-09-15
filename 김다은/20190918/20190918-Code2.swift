ff22852fe2b9a901935f9a0743b31f99989e0e70
DAEUN28 <acone1128@gmail.com>
2019-09-15 13:52:17 +0900

    Update 20190918-Code2.swift

--- 김다은/20190918/20190918-Code2.swift
+++ 김다은/20190918/20190918-Code2.swift
@@ -1,5 +1,5 @@
 //
-//  20190828-Codeswift.swift
+//  20190918-Code2swift.swift
 //  
 //
 //  Created by baby1234 on 22/08/2019.
@@ -7,8 +7,8 @@
 
 import Foundation
 
-func solution(_ x:Int) -> Bool {
-    var result = 0
-    for i in String(x).indices { result += Int("\(String(x)[i])")! }
-    return x % result == 0
+func solution(_ n:Int) -> Int64 {
+    var result = [1,2]
+    for i in 0...n-1 where result.count < n { result.append(result[i] + result [i+1] % 1234567) }
+    return Int64(result[n-1])
 }
