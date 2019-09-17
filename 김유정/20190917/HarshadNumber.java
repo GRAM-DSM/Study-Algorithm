609604c3a551ca08ac07e5cb2c118560e6d55329
Kim Yujeong <37693589+KimYuJeongg@users.noreply.github.com>
2019-09-17 20:26:20 +0900

    Rename HarshadNumber.java

--- 김유정/20190917/HarshadNumber.java
+++ 김유정/20190917/HarshadNumber.java
@@ -1,6 +1,6 @@
 package test;
 
-public class Solution {
+public class HarshadNumber {
 	public boolean solution(int x) {
 	      boolean answer = true;	      
 			int digit1, digit2 = 0, digit3 = 0, digit4 = 0;
@@ -44,7 +44,7 @@ public class Solution {
 	  }
 	
 	public static void main(String[] args) {	
-		Solution s = new Solution();
+		HarshadNumber s = new HarshadNumber();
 		System.out.println(s.solution(10));
 		System.out.println(s.solution(12));
 		System.out.println(s.solution(11));
