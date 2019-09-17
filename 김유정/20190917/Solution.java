73fb5cc95199334f04bc46009a28c2185d5dcc97
Kim Yujeong <37693589+KimYuJeongg@users.noreply.github.com>
2019-09-17 12:34:36 +0900

    Create Solution.java

---
+++ 김유정/20190917/Solution.java
@@ -0,0 +1,54 @@
+package test;
+
+public class Solution {
+	public boolean solution(int x) {
+	      boolean answer = true;	      
+			int digit1, digit2 = 0, digit3 = 0, digit4 = 0;
+			
+			if(x < 10)
+				answer = false;
+			else if(x == 100 || x == 1000 || x == 10000)
+				answer = true;
+			else {
+				if(x <= 99) {
+					digit1 = x / 10;
+					digit2 += x - (digit1 * 10);
+					if(x % (digit1 + digit2) == 0)
+						answer = true;
+					else
+						answer = false;				
+				}
+				else if(x >= 99 && x <= 999) {
+					digit1 = x / 100;
+					digit2 = (x - (digit1 * 100)) / 10;
+					digit3 = x - (digit1 * 100 + digit2 * 10);
+					if(x % (digit1 + digit2 + digit3) == 0)
+						answer = true;
+					else {
+						answer = false;
+					}	
+				}
+				else if(x >= 999 && x <= 9999) {
+					digit1 = x / 1000;
+					digit2 = (x - (digit1 * 1000)) / 100;
+					digit3 = (x -(digit1 * 1000 + digit2 * 100)) / 10 ;
+					digit4 = x -(digit1 * 1000 + digit2 * 100 + digit3 * 10);
+					if(x % (digit1 + digit2 + digit3 + digit4) == 0)
+						answer = true;
+					else {
+						answer = false;
+					}	
+				}
+			}
+	      return answer;
+	  }
+	
+	public static void main(String[] args) {	
+		Solution s = new Solution();
+		System.out.println(s.solution(10));
+		System.out.println(s.solution(12));
+		System.out.println(s.solution(11));
+		System.out.println(s.solution(13));
+	}
+
+}
